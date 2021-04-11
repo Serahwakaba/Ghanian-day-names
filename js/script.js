8507a58a177d74e81e0e506159485bb6b235f619
Serah wakaba <serahwakaba9@gmail.com>
2021-04-11 14:02:17 +0300

    declare names commit

---
+++ js/script.js
@@ -0,0 +1,3 @@
+var days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+var maleAkan = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
