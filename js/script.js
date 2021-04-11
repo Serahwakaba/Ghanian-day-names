6e4201973ef0beac948b55043f0a26272989c330
Serah wakaba <serahwakaba9@gmail.com>
2021-04-11 15:11:25 +0300

    calculations function commit

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@ var days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Satur
 var maleAkan = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var CC, YY, MM, DD, day, dayresult;
+
+function calculations(){
+    
+}
  
