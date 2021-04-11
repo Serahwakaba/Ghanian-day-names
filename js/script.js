be3157b1204665297761f3b2011a2504ef37c22a
Serah wakaba <serahwakaba9@gmail.com>
2021-04-11 15:00:45 +0300

    declare daysvalues commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var days = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 var maleAkan = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var CC, YY, MM, DD, day, dayresult;
+ 
