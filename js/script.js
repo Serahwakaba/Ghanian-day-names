89ae1095d9d26f6b4c5c183ca5512be3b27a065d
Serah wakaba <serahwakaba9@gmail.com>
2021-04-11 15:15:37 +0300

    century and year  commit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ var femaleAkan = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var CC, YY, MM, DD, day, dayresult;
 
 function calculations(){
-    
+  year = document.getElementById("year").value;
+  CC = parseInt(year.substring(0,2));
+  YY = parseInt(year.substring(2,4));
 }
  
